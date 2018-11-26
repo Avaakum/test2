5b1f0082131bf5663e8878dfe7f52855237fe929
Nikita <pishkin1112@gmail.com>
2018-11-26 19:42:14 +0300

    Коммит №2 Испр. script.js и доб. readme

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
-let chooseBtn = document.getElementById('choose'),
-		receiveBtn = document.getElementById('receive'),
+let receiveBtn = document.getElementById('receive'),
+		/*chooseBtn = document.getElementById('choose'),*/
 		heading = document.getElementsByTagName('h2')[0],
 		nameInput = document.getElementsByClassName('contactform_name')[0],
-		phoneInput = document.querySelector('.contactform_phone'),
-		mailInput = document.querySelectorAll('.contactform_mail')[0],
+		// phoneInput = document.querySelector('.contactform_phone'),
+		// mailInput = document.querySelectorAll('.contactform_mail')[0],
 		modal = document.querySelector('.modal'),
-		close = document.querySelector('.close'),
+		closeBtn = document.querySelector('.close'),
 		text = document.getElementsByName('message')[0];
 
 function hover() {
-	heading.textContent = "Мы не шутим, правда ВСЁ!"
+	heading.textContent = "Мы не шутим, правда ВСЁ!";
 }
 
-function out() {
-	heading.textContent = "Все включено даже сейчас!"
-}
+// function out() {
+// 	heading.textContent = "Все включено даже сейчас!";
+// }
 
 
 heading.addEventListener("click", hover);
 heading.addEventListener("dblclick", function() {
-	heading.textContent = "Все включено даже сейчас!"
+	heading.textContent = "Все включено даже сейчас!";
 });
 
 
 receiveBtn.addEventListener("click", function() {
-	modal.style.display = "block"
+	modal.style.display = "block";
 });
 
-close.addEventListener("click", function() {
-	modal.style.display = "none"
+closeBtn.addEventListener("click", function() {
+	modal.style.display = "none";
 });
 
 nameInput.addEventListener("input", function() {
-	text.value = nameInput.value + ": "
+	text.value = nameInput.value + ": ";
 
 	if (nameInput.value == "") {
-		text.value = ""
+		text.value = "";
 	}
 });
 
